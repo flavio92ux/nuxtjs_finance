4ebe086a34ba896dc4fdec981dc0230fd0047cd3
Flavio Franco <flavio.win@gmail.com>
2022-02-27 10:40:15 -0300

    criando header e pagina de servicos e empresa

---
+++ pages/a-empresa.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="bg-gray-100">
+      <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
+        <NuxtLink :to="{ name: 'a-empresa' }">A empresa</NuxtLink>
+        <NuxtLink to="/servicos">Serviços</NuxtLink>
+      </div>
+    </div>
+
+    <div>
+      <div class="container mx-auto">
+        <h1 class="text-lg font-bold">A empresa</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore nesciunt, facilis facere cumque, neque voluptatum magnam nam commodi suscipit beatae assumenda ad quae laborum labore. Minus quibusdam accusamus ducimus voluptas.</p>
+      </div>
+    </div>
+  </div>
+</template>
